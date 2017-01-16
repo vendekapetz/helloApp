9957b36a602b0665c2d32c9add13d28b45fc8a46
Admin0 <admin0@localhost.localdomain>
2017-01-16 14:45:01 +0200

    upload helloApp

---
+++ src/main/java/org/host/hello/App.java
@@ -0,0 +1,13 @@
+package org.host.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
